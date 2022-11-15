6be7f8788e2e3565072ea86811ce6223d2acaf92
AFaidley <alviva.faidley@gmail.com>
2022-11-14 20:52:29 -0700

    Worked on app.js, and started on signup form

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// Create main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Create req middleware that attachs JWT token to every req as an auth header
+const authLink = setContext((_, { headers }) => {
+  // Get the auth token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // Return headers to the context so it can be read by httplink
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -25,6 +56,7 @@ function App() {
         </Routes>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
