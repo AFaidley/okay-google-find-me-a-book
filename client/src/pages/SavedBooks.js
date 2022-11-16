7a8a152a50236bf0c3caadad2c9fa70ee1acc4c5
AFaidley <alviva.faidley@gmail.com>
2022-11-15 18:09:21 -0700

    Worked on Searchbooks, started savdbooks

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
-import { getMe, deleteBook } from '../utils/API';
+import { REMOVE_BOOK } from "../utils/mutations";
+import { GET_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
